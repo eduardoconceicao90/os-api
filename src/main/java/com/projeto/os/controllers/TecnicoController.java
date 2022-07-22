2bd4cd17c3bc4047e6c992783c39c8d017b567a0
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-22 10:56:32 -0300

    POST Tecnico

--- src/main/java/com/projeto/os/controllers/TecnicoController.java
+++ src/main/java/com/projeto/os/controllers/TecnicoController.java
@@ -1,5 +1,6 @@
 package com.projeto.os.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projeto.os.dtos.TecnicoDTO;
+import com.projeto.os.entities.Tecnico;
 import com.projeto.os.services.TecnicoService;
 
 @RestController
@@ -31,5 +36,12 @@ public class TecnicoController {
 		List<TecnicoDTO> listDTO = tecnicoService.findAll()
 				.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO){
+		Tecnico newObj = tecnicoService.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/projeto/os/services/TecnicoService.java
+++ src/main/java/com/projeto/os/services/TecnicoService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projeto.os.dtos.TecnicoDTO;
 import com.projeto.os.entities.Tecnico;
 import com.projeto.os.repositories.TecnicoRepository;
 import com.projeto.os.services.exceptions.ObjectNotFoundException;
@@ -15,15 +16,19 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository tecnicoRepository;
-		
+
 	public Tecnico findById(Long id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado Id: " + id + ", tipo: " + Tecnico.class.getName()));
 	}
-	
-	public List<Tecnico> findAll(){
+
+	public List<Tecnico> findAll() {
 		return tecnicoRepository.findAll();
 	}
-	
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		return tecnicoRepository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
+	}
+
 }
