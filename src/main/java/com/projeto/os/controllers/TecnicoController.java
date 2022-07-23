ab5d14ae8fe5e9713ee7b7c3042dfbd1d2bd2339
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-07-23 11:32:15 -0300

    Deploy Heroku

--- src/main/java/com/projeto/os/controllers/TecnicoController.java
+++ src/main/java/com/projeto/os/controllers/TecnicoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.projeto.os.dtos.TecnicoDTO;
 import com.projeto.os.entities.Tecnico;
 import com.projeto.os.services.TecnicoService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoController {
